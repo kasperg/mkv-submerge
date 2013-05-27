c8ecbbd4c9f675123c553ace383d0754428c38dd
Kasper Garnaes <kasper.garnaes@gmail.com>
2013-05-28 01:09:31 +0200

    Add code comments

--- src/MkvSubmerge/MergeCommand.php
+++ src/MkvSubmerge/MergeCommand.php
@@ -66,64 +66,89 @@ class MergeCommand extends Command
         $lang = $input->getOption('lang');
         $cleanup = $input->getOption('cleanup');
 
-        $this->write($output, '<comment>Searching for video files in ' . $dir . '</comment>', TRUE);
+        $this->write($output, '<comment>Searching for video files in ' . $dir . '</comment>', true);
         $files = Finder::create()->in($dir)->name('/\.(avi|mkv)$/')->notName('*.subs.mkv')->sortByName()->files();
         foreach ($files as $file) {
-            $this->write($output, '<comment>Processing ' . $file->getRealPath() . '</comment>', TRUE);
+            $this->write($output, '<comment>Processing ' . $file->getRealPath() . '</comment>', true);
 
+            // Having the base name available is useful when determining other
+            // file names as they should be derivatives of this.
             $baseName = substr($file->getFilename(), 0, strrpos($file->getFilename(), '.'));
 
+            // Build the command which will try to download subtitles for the
+            // file. Use --force so that we always retrieve a fresh version.
             $periscopeCommand = array(
-              $periscope,
-              escapeshellarg($file->getRealPath()),
-              '-l ' . $lang,
-              '--force');
+                $periscope,
+                escapeshellarg($file->getRealPath()),
+                '-l ' . $lang,
+                '--force'
+            );
             $periscopeCommand = implode(' ', $periscopeCommand);
-            $this->write($output, $periscopeCommand, TRUE, OutputInterface::VERBOSITY_VERBOSE);
+            $this->write($output, $periscopeCommand, true, OutputInterface::VERBOSITY_VERBOSE);
             $periscopeProces = new \Symfony\Component\Process\Process($periscopeCommand);
             $periscopeProces->run();
 
-            $this->write($output, $periscopeProces->getErrorOutput(), TRUE, OutputInterface::VERBOSITY_VERBOSE);
+            // Periscope always seems to return output with an error return type
+            // so grab the output from there.
+            $this->write($output, $periscopeProces->getErrorOutput(), true, OutputInterface::VERBOSITY_VERBOSE);
+            // To determine whether any subtitles were downloaded we search
+            // through the output.
             preg_match(
                 '/Downloaded (\d+) subtitles/i',
                 $periscopeProces->getErrorOutput(),
                 $matches
             );
-            $this->write($output, $matches[1] . ' subtitle file(s) found', TRUE);
+            $this->write($output, $matches[1] . ' subtitle file(s) found', true);
 
+            // If we found subtitles then merge them in.
             if ($matches[1] > 0) {
-                $this->write($output, 'Merging subtitles into MKV file', TRUE);
+                $this->write($output, 'Merging subtitles into MKV file', true);
+                // Build the command which will merge the movie and subtitle
+                // files into one MKV file.
                 $mkvMergeCommand = array(
-                  $mkvMerge,
-                  '-o ' . escapeshellarg($file->getPath() . '/' . $baseName . '.subs.mkv'),
-                  '--default-track 0',
-                  '--language 0:' .$lang,
-                  escapeshellarg($file->getPath() . '/' . $baseName . '.srt'),
-                  escapeshellarg($file->getRealPath()),
+                    $mkvMerge,
+                    '-o ' . escapeshellarg($file->getPath() . '/' . $baseName . '.subs.mkv'),
+                    '--default-track 0',
+                    '--language 0:' .$lang,
+                    escapeshellarg($file->getPath() . '/' . $baseName . '.srt'),
+                    escapeshellarg($file->getRealPath()),
                 );
                 $mkvMergeCommand = implode(' ', $mkvMergeCommand);
-                $this->write($output, $mkvMergeCommand, TRUE, OutputInterface::VERBOSITY_VERBOSE);
+                $this->write($output, $mkvMergeCommand, true, OutputInterface::VERBOSITY_VERBOSE);
                 $mkvProcess = new Process($mkvMergeCommand);
+                // Set timeout for the merge process to 10 minutes. It should
+                // not take so long but we want to be safe.
                 $mkvProcess->setTimeout(600);
+                // In PHP 5.3 we cannot rename closure variables so we reference
+                // $this as $command to allow the closure to generate some
+                // output.
                 $command = $this;
                 $mkvProcess->run(
                     function ($type, $buffer) use($command, $output) {
+                      // If the output starts with Progress it contains a
+                      // progress indicator which we want to show. Otherwise
+                      // the output is primarily for debugging.
                       $verbose = (stripos($buffer, 'Progress') === 0) ? OutputInterface::VERBOSITY_NORMAL : OutputInterface::VERBOSITY_VERBOSE;
-                      $command->write($output, $buffer, FALSE, $verbose);
+                      $command->write($output, $buffer, false, $verbose);
                     }
                 );
-                $this->write($output, '', TRUE);
+                // We did not include a linebreak above to do it here such that
+                // we have proper separation of lines.
+                $this->write($output, '', true);
 
+                // Determine whether the merge went well or not.
                 $mergeSuccess = $mkvProcess->getExitCodeText() == 'OK';
                 if ($mergeSuccess) {
-                    $this->write($output, '<info>Merge complete for ' . $file->getRealPath() . '</info>', TRUE);
+                    $this->write($output, '<info>Merge complete for ' . $file->getRealPath() . '</info>', true);
                 } else {
-                    $this->write($output, $mkvProcess->getOutput(), TRUE);
-                    $this->write($output, '<error>Merge error for ' . $file->getRealPath() . ': ' . $mkvProcess->getExitCodeText() . '</error>', TRUE);
+                    $this->write($output, $mkvProcess->getOutput(), true);
+                    $this->write($output, '<error>Merge error for ' . $file->getRealPath() . ': ' . $mkvProcess->getExitCodeText() . '</error>', true);
                 }
 
                 if (($cleanup == 'merge-success' && $mergeSuccess) ||
                     ($cleanup == 'merge-complete' && is_file($file->getPath() . '/' . $baseName . '.subs.mkv'))) {
+                    // If cleanup criteria are met then delete subtitle file
+                    // downloaded by periscope and the original movie file.
                     $this->write($output, '<comment>Cleaning up original files</comment>', true);
                     unlink($file->getPath() . '/' . $baseName . '.srt');
                     unlink($file->getRealPath());
@@ -132,7 +157,21 @@ class MergeCommand extends Command
         }
     }
 
-    public function write(OutputInterface $output, $messages, $newline = FALSE, $verbosity = OutputInterface::VERBOSITY_NORMAL, $type = OutputInterface::OUTPUT_NORMAL) {
+    /**
+     * Helper method which supports outputting messages with different verbosity
+     * levels.
+     *
+     * The method is public because we want to call it from closures.
+     *
+     * @param OutputInterface $output   The interface to write the messages to.
+     * @param string|array $messages    The message as an array of lines or a single string
+     * @param bool $newline             Whether to add a newline at the end of the message(s)
+     * @param int $verbosity            The verbosity level for the messages.
+     * @param int $type                 The message type.
+     */
+    public function write(OutputInterface $output, $messages, $newline = false, $verbosity = OutputInterface::VERBOSITY_NORMAL, $type = OutputInterface::OUTPUT_NORMAL) {
+        // Only output the messages if their verbosity is equal to or higher
+        // than what has been requested.
         if ($output->getVerbosity() >= $verbosity) {
             $output->write($messages, $newline, $type);
         }
